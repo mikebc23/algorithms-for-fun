11ddd9c567eed5eda3e19495022a9c36ce75220c
Michael Barquero <michaelbc@me.com>
2020-04-19 18:56:27 -0400

    Fixing a bug in QuickSort

--- basics/Sorting/PHP/QuickSort.php
+++ basics/Sorting/PHP/QuickSort.php
@@ -6,19 +6,24 @@
 * Complexity: O (n log n) 
 */
 
-function quickSort($dataset, $first, $last):
+$numbers = [20, 6, 8, 53, 56, 23, 87, 41, 49, 19];
+
+function quickSort($dataset, $first, $last) {
+    
     if($first < $last) {
         # calculate the split point
         $pivotIdx = partition($dataset, $first, $last);
 
         # now sort the two partitions
-        quickSort($dataset, $first, $pivotIdx-1);
-        quickSort($dataset, $pivotIdx+1, $last);
+        $dataset = quickSort($dataset, $first, $pivotIdx-1);
+        $dataset = quickSort($dataset, $pivotIdx+1, $last);
+    
+        return $dataset;
+
     }
 }
 
-
-funcion partition($datavalues, $first, $last) {
+function partition($datavalues, $first, $last) {
     # choose the first item as the pivot value
     $pivotvalue = $datavalues[$first];
     # establish the upper and lower indexes
@@ -27,19 +32,19 @@ funcion partition($datavalues, $first, $last) {
 
     # start searching for the crossing point
     $done = false;
-    while(!$done) {
+    while($done === false) {
         # advance the lower index
-        while (($lower <= $upper) && $datavalues[$lower] <= $pivotvalue)){
+        while (($lower <= $upper) && ($datavalues[$lower] <= $pivotvalue)){
             $lower += 1;
         }
 
         # advance the upper index
         while (($datavalues[$upper] >= $pivotvalue) && ($upper >= $lower)) {
-            upper -= 1;
+            $upper -= 1;
         }
 
         # if the two indexes cross, we have found the split point
-        if ($upper < $lower{
+        if ($upper < $lower){
             $done = true;
         }else{
             # exchange the two values
@@ -58,9 +63,8 @@ funcion partition($datavalues, $first, $last) {
     return $upper;
 }
 
-$numbers = [20, 6, 8, 53, 56, 23, 87, 41, 49, 19];
 
-# test the merge sort with data
-print(f"Original: {numbers}")
-quickSort(numbers, 0, len(numbers)-1)
-print(f"Result: {numbers}")
+echo "Original: " . json_encode($numbers) . "\n";
+
+$result = quickSort($numbers, 0, count($numbers)-1);
+echo "Result:   " . json_encode($result) . "\n";
